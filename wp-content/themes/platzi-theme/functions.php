a3a30441527ba151c34f1a28644b2c353cf632b4
Rafa Poveda <raven@raven.es>
2017-07-17 19:18:17 -0500

    creamos el CPT 'viaje'

--- wp-content/themes/platzi-theme/functions.php
+++ wp-content/themes/platzi-theme/functions.php
@@ -17,3 +17,43 @@ function add_role_viajero()
  
 // add the simple_role
 add_action('init', 'add_role_viajero');
+
+
+function viajes_init() {
+  $labels = array(
+    'name'               => _x( 'Viajes', 'post type general name', 'your-plugin-textdomain' ),
+    'singular_name'      => _x( 'viaje', 'post type singular name', 'your-plugin-textdomain' ),
+    'menu_name'          => _x( 'Mis viajes', 'admin menu', 'your-plugin-textdomain' ),
+    'name_admin_bar'     => _x( 'Viajes', 'add new on admin bar', 'your-plugin-textdomain' ),
+    'add_new'            => _x( 'Añadir nuevo', 'viaje', 'your-plugin-textdomain' ),
+    'add_new_item'       => __( 'Añadir nuevo viaje', 'your-plugin-textdomain' ),
+    'new_item'           => __( 'Nuevo viaje', 'your-plugin-textdomain' ),
+    'edit_item'          => __( 'Editar viaje', 'your-plugin-textdomain' ),
+    'view_item'          => __( 'Ver viaje', 'your-plugin-textdomain' ),
+    'all_items'          => __( 'Todos los viajes', 'your-plugin-textdomain' ),
+    'search_items'       => __( 'Buscar viajes', 'your-plugin-textdomain' ),
+    'parent_item_colon'  => __( 'Viajes padre:', 'your-plugin-textdomain' ),
+    'not_found'          => __( 'No hemos encontrado viajes.', 'your-plugin-textdomain' ),
+    'not_found_in_trash' => __( 'No hemos encontrado viajes en la papelera.', 'your-plugin-textdomain' )
+  );
+
+  $args = array(
+    'labels'             => $labels,
+    'description'        => __( 'Description.', 'your-plugin-textdomain' ),
+    'public'             => true,
+    'publicly_queryable' => true,
+    'show_ui'            => true,
+    'show_in_menu'       => true,
+    'query_var'          => true,
+    'rewrite'            => array( 'slug' => 'viaje' ),
+    'capability_type'    => 'post',
+    'has_archive'        => true,
+    'hierarchical'       => false,
+    'menu_position'      => null,
+    'menu_icon'          => 'dashicons-admin-multisite',  
+    'supports'           => array( 'title', 'editor', 'author', 'thumbnail' )
+  );
+
+  register_post_type( 'viaje', $args );
+}
+add_action( 'init', 'viajes_init' );
